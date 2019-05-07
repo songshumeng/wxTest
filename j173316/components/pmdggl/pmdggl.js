719c1e7b22252640d85ccca8d9611660014e1196
songshumeng <48705570+songshumeng@users.noreply.github.com>
2019-05-07 10:39:14 +0800

    Add files via upload

--- j173316/components/pmdggl/pmdggl.js
+++ j173316/components/pmdggl/pmdggl.js
@@ -12,13 +12,13 @@ Component({
    */
   data: {
     imgList: [{
-      imagePath: "/images/home.png",
+      imagePath: "/images/zdj1.jpg",
       pagePath: "/pages/calc/calc"
     }, {
-      imagePath: "/images/home2.png",
+        imagePath: "/images/zdj2.jpg",
       pagePath: "/pages/lxjsq/lxjsq"
     }, {
-      imagePath: "/images/home3.png",
+        imagePath: "/images/zdj3.jpg",
       pagePath: "/pages/lxjsq/lxjsq"
     }
     ]
